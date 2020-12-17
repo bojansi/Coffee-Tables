b79a3653698ae517e91d897da0d0a72f31544b60
Bojan Simić <72159059+bojansi@users.noreply.github.com>
2020-12-17 16:48:54 +0100

    Kreirane model-klase koje odgovaraju zadatim tabelama.

---
+++ CoffeeTables/DataLayer/Models/Receipt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class Receipt
+    {
+
+        public int Id { get; set; }
+        public int WaiterId { get; set; }
+        public int TableId { get; set; }
+        public int Date { get; set; }
+        public decimal Cost { get; set; }
+
+
+        
+    }
+}
