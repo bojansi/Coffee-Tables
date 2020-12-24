be622a5af4841cc88611c84c0830d9b848d3e447
Željko <zeljko.milovanovic0611765038@gmail.com>
2020-12-24 01:55:16 +0100

    Napravljene sve metode u ProductBusiness klasi

--- CoffeeTables/BusinessLayer/ProductBusiness.cs
+++ CoffeeTables/BusinessLayer/ProductBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataLayer;
+using DataLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,42 @@ namespace BusinessLayer
 {
     public class ProductBusiness
     {
+        private readonly ProductRepository productRepository;
+        public ProductBusiness()
+        {
+            this.productRepository = new ProductRepository();
+        }
+        public List<Product> getAllProduct()
+        {
+            return this.productRepository.GetAllProducts();
+        }
+        public bool insertProduct(Product p)
+        {
+            if(this.productRepository.InsertProduct(p) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool updateProduct(Product p)
+        {
+            if (this.productRepository.UpdateProduct(p) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool removeProduct(int Id)
+        {
+            if (this.productRepository.DeleteProduct(Id) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public Product getProductById(int Id)
+        {
+            return this.productRepository.GetAllProducts().FirstOrDefault(p => p.Id == Id);
+        }
     }
 }
