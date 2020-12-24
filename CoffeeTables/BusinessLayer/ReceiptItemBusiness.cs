f73c346d2fe2492f14f9754fa00ef15b13bc358a
Željko <zeljko.milovanovic0611765038@gmail.com>
2020-12-24 02:52:42 +0100

    Kreirane metode za ReceiptBusiness i ReceiptItemBusiness

--- CoffeeTables/BusinessLayer/ReceiptItemBusiness.cs
+++ CoffeeTables/BusinessLayer/ReceiptItemBusiness.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataLayer;
+using DataLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,38 @@ namespace BusinessLayer
 {
     public class ReceiptItemBusiness
     {
+        private readonly ReceiptItemRepository receiptItemRepository;
+        public ReceiptItemBusiness()
+        {
+            this.receiptItemRepository = new ReceiptItemRepository();
+        }
+        public List<ReceiptItem> getAllReceiptItems()
+        {
+            return this.receiptItemRepository.GetAllReceiptItems();
+        }
+        public bool insertReceiptItem(ReceiptItem r)
+        {
+            if (this.receiptItemRepository.InsertReceiptItem(r) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool updateReceiptItem(ReceiptItem r)
+        {
+            if (this.receiptItemRepository.UpdateReceiptItem(r) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool deleteReceiptItemById(int rId, int pId)
+        {
+            if (this.receiptItemRepository.DeleteReceiptItemById(rId,pId) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
     }
 }
