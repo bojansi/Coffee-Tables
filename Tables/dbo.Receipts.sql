aa7ee879d194174213c4fb1c3d2c410962c2e731
Bojan Simić <72159059+bojansi@users.noreply.github.com>
2020-12-26 12:25:57 +0100

    Izmenjen tip podatka u tabeli Receipt, kao i greska u definisanju prenesenog kljuca u tabeli ReceiptItem.

--- Tables/dbo.Receipts.sql
+++ Tables/dbo.Receipts.sql
@@ -2,9 +2,9 @@
     [Id]       INT          IDENTITY (1, 1) NOT NULL,
     [WaiterId] INT          NOT NULL,
     [TableId]  INT          NOT NULL,
-    [Date]     DATE         NOT NULL,
+    [Date]     DATETIME         NOT NULL,
     [Total]    DECIMAL (18) NOT NULL,
-    [Paid]     BIT          NOT NULL DEFAULT 0,
+    [Paid]     BIT          DEFAULT ((0)) NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_Receipts_ToWaiters] FOREIGN KEY ([WaiterId]) REFERENCES [dbo].[Waiters] ([Id]),
     CONSTRAINT [FK_Receipts_ToTables] FOREIGN KEY ([TableId]) REFERENCES [dbo].[Tables] ([Id])
